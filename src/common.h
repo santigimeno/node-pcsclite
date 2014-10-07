2d11f57d95c887c873475eed898f2d635a038ebf
Santiago Gimeno <santiago.gimeno@quantion.es>
2014-10-07 17:04:13 +0200

    Windows fixes

--- src/common.h
+++ src/common.h
@@ -6,11 +6,19 @@
 namespace {
     std::string error_msg(const char* method, LONG result) {
         char msg[ERR_MSG_MAX_LEN];
+#ifdef _WIN32
+        _snprintf(msg,
+                 ERR_MSG_MAX_LEN,
+                 "%s error: 0x%.8lx",
+                 method,
+                 result);
+#else
         snprintf(msg,
                  ERR_MSG_MAX_LEN,
                  "%s error: %s(0x%.8lx)",
                  method,
                  pcsc_stringify_error(result), result);
+#endif
         return msg;
     }
 }
